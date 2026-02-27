e611a1f68c66e7e9eefdc8cca9ec0ad32fbd91cb
Jesús Rodríguez Marín <jesus.rodriguez.marin@ces-vegamedia.es>
2026-02-27 11:48:11 +0100

    Añadido proyecto pruebaHub

---
+++ pruebaHub/src/pruebaHub/prueba/LaPruebaDefinitiva.java
@@ -0,0 +1,14 @@
+package pruebaHub.prueba;
+
+public class LaPruebaDefinitiva {
+
+	public static void main(String[] args) {
+		// Creamos un array de String
+		String Array[] = {"Al", "Fin", "Listo"};
+		
+		for (int i=0; i<Array.length; i++) {
+			System.out.print(Array[i]+" ");
+		}
+	}
+
+}

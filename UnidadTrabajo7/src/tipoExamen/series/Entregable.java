a02e60fb0b402abd6fbcc9f9e71387b825d03a21
Jesús Rodríguez Marín <jesus.rodriguez.marin@ces-vegamedia.es>
2026-03-17 11:36:16 +0100

    Series y videojuegos: a mitad

---
+++ UnidadTrabajo7/src/tipoExamen/series/Entregable.java
@@ -0,0 +1,9 @@
+package tipoExamen.series;
+
+public interface Entregable {
+
+	// Métodos que se implementarán en las clases con implements
+	public void entregar();
+	public void devolver();
+	public boolean isPrestado();
+}
---
+++ UnidadTrabajo7/src/tipoExamen/series/Videojuego.java
@@ -0,0 +1,68 @@
+package tipoExamen.series;
+
+public class Videojuego implements Entregable {
+
+	// Constante 
+	private final static int HORAS_ESTIMADAS_DEF=100;	// Nº de horas estimadas por defecto
+	
+	// Atributos
+	private String titulo;
+	private int horasEstimadas;
+	private String genero;
+	private String desarrollador;
+	private boolean presatado;
+	
+	/**
+	 * Constructor por defecto
+	 */
+	public Videojuego() {
+		this("", HORAS_ESTIMADAS_DEF, "", "");
+	}
+	
+	/**
+	 * Constructor con dos parámetros
+	 * @param titulo
+	 * @param desarrollador
+	 */
+	public Videojuego(String titulo, String desarrollador) {
+		this(titulo, HORAS_ESTIMADAS_DEF, "", desarrollador);
+	}
+	
+	/**
+	 * Constructor con todos los parámetros
+	 * @param titulo
+	 * @param horasEstimadas
+	 * @param genero
+	 * @param desarrollador
+	 * @param presatado
+	 */
+	public Videojuego(String titulo, int horasEstimadas, String genero, String desarrollador, boolean presatado) {
+		this.titulo = titulo;
+		this.horasEstimadas = horasEstimadas;
+		this.genero = genero;
+		this.desarrollador = desarrollador;
+		this.presatado = presatado;
+	}
+	
+	
+
+	@Override
+	public void entregar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void devolver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPrestado() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+}

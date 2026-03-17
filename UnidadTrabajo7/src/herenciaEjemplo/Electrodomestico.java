7fd2e0ad9efaf8e96d27536a1ab5657429817b2b
Jesús Rodríguez Marín <jesus.rodriguez.marin@ces-vegamedia.es>
2026-03-17 10:18:17 +0100

    herenciaYpolimorfismo

--- UnidadTrabajo7/src/herenciaEjemplo/Electrodomestico.java
+++ UnidadTrabajo7/src/herenciaEjemplo/Electrodomestico.java
@@ -1,4 +1,4 @@
-package electrodomestico;
+package herenciaEjemplo;
 
 public class Electrodomestico {
 
@@ -33,7 +33,6 @@ public class Electrodomestico {
 	 * @param peso       {double} Peso del electrodoméstico
 	 */
 	public Electrodomestico(double precioBase, double peso) {
-		this();
 		this.precioBase = precioBase;
 		this.peso = peso;
 	}
@@ -96,6 +95,7 @@ public class Electrodomestico {
 
 	/**
 	 * Comprueba el consumo energético
+	 * 
 	 * @param letra {char} correspondiente al consumo del electrodoméstico
 	 */
 	private void comprobarConsumoElectrico(char letra) {
@@ -106,10 +106,64 @@ public class Electrodomestico {
 			this.consumoEnergetico = CONSUMO_DEF;
 		}
 	}
-	
+
 	private void comprobarColorElectrodomestico(String color) {
-		String colores[] = {};
-		
+		String colores[] = { "blanco", "negro", "rojo", "azul", "gris" };
+		boolean encontrado = false;
+
+		for (int i = 0; i < colores.length && !encontrado; i++) {
+			if (colores[i].equals(color))
+				encontrado = true;
+		}
+
 	}
+
+	/**
+	 * Calcula y devuelve el precio final del electrodméstico
+	 * 
+	 * @return {double} devuelve el precio final del electrodméstico
+	 */
+	public double precioFinal() {
+		double plus = 0;
+		switch (this.consumoEnergetico) {
+		case 'A': plus = 100;
+
+			break;
+		case 'B': plus = 80;
+
+			break;
+		case 'C': plus = 60;
+
+			break;
+		case 'D': plus = 50;
+
+			break;
+		case 'E': plus = 30;
+
+			break;
+		case 'F': plus = 10;
+
+			break;
+		}
+		
+		if (peso>= 0 && peso <= 19)
+			plus += 10;
+		else if (peso >= 20 && peso <= 49)
+			plus +=50;
+		else if (peso >= 50 && peso <= 79)
+			plus +=80;
+		else if (peso >= 80)
+			plus += 100;
+		return this.precioBase + plus;
+	}
+
+	/**
+	 * Método para devolver información del electrodoméstico
+	 */
+	@Override
+	public String toString() {
+		return "Electrodomestico: color "+color+", peso"+peso+" kg, consumo energético "+consumoEnergetico+", precio final "+this.precioFinal()+" euros";
+	}
+	
 
 }
---
+++ UnidadTrabajo7/src/herenciaEjemplo/Television.java
@@ -0,0 +1,71 @@
+package herenciaEjemplo;
+
+public class Television extends Electrodomestico {
+
+	// Constantes
+	private final static int RESOLUCION_DEF = 20;
+
+	// Atributo
+	private int resolucion; // Carga de la lavadora
+
+	public Television() {
+			super();
+			this.resolucion=RESOLUCION_DEF;
+		}
+
+	public Television(double precioBase, double peso) {
+			this(RESOLUCION_DEF, precioBase, peso, CONSUMO_DEF, COLOR_DEF);
+		}
+
+	/**
+		 * Constructor
+		 * LLamamos al constructor super() con 4 parámetros, se los introducimos para
+		 * invocarlo
+		 * 
+		 * @param carga {int}
+		 * @param precioBase {double}
+		 * @param peso {double}
+		 * @param consumoEnergetico {char}
+		 * @param color {String}
+		 */
+		public Television(int carga, double precioBase, double peso, char consumoEnergetico, String color) {
+			super(precioBase, peso, consumoEnergetico, color);
+			this.resolucion = resolucion;
+		}
+
+	/**
+	 * Devuelve la carga de la lavadora
+	 * 
+	 * @return the carga {int}
+	 */
+	public int getResolucion() {
+		return resolucion;
+	}
+	
+
+
+	/**
+	 * Calcula y devuelve el precio final de la lavadora
+	 * 
+	 * @return {double} precio final de la lavadora
+	 */
+	public double precioFinal() {
+		// Invocamos el método precio final del padre super.precioFinal();
+		double preciofin = super.precioFinal();
+
+		if (resolucion > 40)
+			preciofin = precioBase*0.3;
+
+		return preciofin;
+	}
+
+	/**
+	 * Método para devolver información del electrodoméstico
+	 */
+	@Override
+	public String toString() {
+		return "Lavadora: color " + color + ", peso" + peso + " kg, consumo energético " + consumoEnergetico
+				+ ", resolucion " + resolucion + ", precio final " + this.precioFinal() + " euros";
+	}
+	arreglar que falla en television
+}

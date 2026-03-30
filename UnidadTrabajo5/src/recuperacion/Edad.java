e775452b14565e1794503a216bd266acb52c9f27
Jesus Rodriguez Marin <jesus.rodriguez.marin@ces-vegamedia.es>
2026-03-30 21:20:33 +0200

    Comienzo reapaso recuparación

---
+++ UnidadTrabajo5/src/recuperacion/Edad.java
@@ -0,0 +1,69 @@
+package recuperacion;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Edad {
+
+	public static void main(String[] args) {
+		
+		// En base a la fecha de hoy y la fecha de nacimiento 
+		// introducida por teclado
+		// determinamos la edad de una persona
+		
+		// Sacamos la fecha de hoy creando una instancia de Date
+		Date hoy = new Date();
+		
+		// Separamos la fecha con Format para trabajar con los números
+		SimpleDateFormat formato;
+		
+		// Creamos instancia para año
+		formato = new SimpleDateFormat("yyyy");
+		String year = formato.format(hoy);
+		
+		// Creamos instancia para mes
+		formato = new SimpleDateFormat("MM");
+		String month = formato.format(hoy);
+		
+		// Creamos instancia para día
+		formato = new SimpleDateFormat("dd");
+		String day = formato.format(hoy);
+		
+		System.out.println("La fecha de hoy es: "+day+"/"+month+"/"+year);
+		
+		// Convertimos los String a int para realizar cálculos
+		int aHoy = Integer.parseInt(year);
+		int mHoy = Integer.parseInt(month);
+		int dHoy = Integer.parseInt(day);
+		
+		// Pedimos fecha de nacimiento al usuario
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n--Introduce tu fecha de nacimiento--");
+		System.out.print("Año: ");
+		int y = sc.nextInt();
+		System.out.print("Mes: ");
+		int m = sc.nextInt();
+		System.out.print("Día: ");
+		int d = sc.nextInt();
+		
+		
+		
+		// Formula para la edad restando al año actual el año introducido
+		int edad = aHoy-y;
+		
+		// Si el mes introcucido es mayor o igual, condicionamos según el día introducido
+		if (mHoy<m) {
+			edad--;
+		} else if (m==mHoy) {
+			if (d>dHoy) {
+				edad--;
+			}
+		}
+		
+		// Mostramos la edad
+		System.out.println("\nTu edad es: "+edad+" años.");
+
+	}
+
+}
---
+++ UnidadTrabajo5/src/recuperacion/EntradaDatos.java
@@ -0,0 +1,31 @@
+package recuperacion;
+
+import java.util.Scanner;
+
+public class EntradaDatos {
+
+	public static void main(String[] args) {
+		
+		// Pedimos datos al usuario
+		Scanner sc = new Scanner(System.in);
+		
+		// Pedimos nombre y leemos en String
+		System.out.print("Introduce tu nombre: ");
+		String nombre = sc.nextLine();
+		
+		// Pedimos edad, leemos en String y parseamos a int para evitar salto de carro
+		System.out.print("Introduce tu edad: ");
+		int edad = Integer.parseInt(sc.nextLine());
+		
+		// Pedimos dirección y leemos con String
+		System.out.print("Introduce tu dirección: ");
+		String direccion = sc.nextLine();
+		
+		// Mostramos información
+		System.out.println(" >Nombre: "+nombre+"\n >Edad: "+edad+"\n >Dirección: "+direccion);
+
+		// Cerramos scanner
+		sc.close();
+	}
+
+}

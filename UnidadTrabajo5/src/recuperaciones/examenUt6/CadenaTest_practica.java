1ab85dd5758f5df1c9a0f406e0cb73e073d2e296
Jesus Rodriguez Marin <jesusrodriguezmarin@MacBook-Air-de-Jesus.local>
2026-04-03 20:00:53 +0200

    Trabajando en POO cadena_practica y cadenaTest_practica, falta parte final -eliminar espacios-

--- UnidadTrabajo5/src/recuperaciones/examenUt6/CadenaTest_practica.java
+++ UnidadTrabajo5/src/recuperaciones/examenUt6/CadenaTest_practica.java
@@ -1,10 +1,57 @@
 package recuperaciones.examenUt6;
 
+import java.util.Scanner;
+
 public class CadenaTest_practica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Instanciamos un objeto cadena
+		Cadena_practica c = new Cadena_practica();
+		
+		// Solicitamos una cadena de texto por teclado
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce tu nombre: ");
+		String nombre = sc.nextLine();
+		sc.close();
+		
+		// Establecemos la cadena solicitada por consola como cadena con -set
+		c.setCadena(nombre);
+		
+		// Mostramos con -get
+		System.out.println(c.getCadena());
+		
+		// Mostramos en mayúsculas con el método creado
+		System.out.println(c.mayus());
+		
+		// Mostramos en minúsculas con el método creado
+		System.out.println(c.minus());
+		
+		// Mostramos la cadena invertida con el método creado
+		System.out.println(c.reverso());
+		
+		// Mostramos la longitud de la cadena
+		System.out.println(c.longi());
+		
+		// Mostramos el números de vocales de la cadena
+		System.out.println(c.voc());
+		
+		// Mostramos el número de consonantes
+		System.out.println(c.cons());
+		
+		// Mostramos el número de apariciones de el caracter introdiucido
+		System.out.println(c.carac('p'));
+		
+		// Reemplazamos un caracter de la cadena por otro
+		c.setCadena(c.mayus());
+		c.reemp('P', 'F');
+		System.out.println(c.getCadena());
+		
+		// Eliminamos el caracter pasado por parámetro
+		c.borra('A');
+		System.out.println(c.getCadena());
+			
+		
 	}
 
 }
--- UnidadTrabajo5/src/recuperaciones/examenUt6/Cadena_practica.java
+++ UnidadTrabajo5/src/recuperaciones/examenUt6/Cadena_practica.java
@@ -37,10 +37,105 @@ public class Cadena_practica {
 	}
 
 	/**
-	 * 
+	 * Devuelve la candena en mayúsculas
+	 * @return {String} la cadena en mayúsculas
 	 */
-	public void mayusculas() {
-		this.cadena=cadena.toUpperCase();
+	public String mayus() {
+		return cadena.toUpperCase();
 	}
 	
+	/**
+	 * Devuelve la cadena en minúsculas
+	 * @return {String} la cadena en minúsculas
+	 */
+	public String minus() {
+		return cadena.toLowerCase();
+	}
+	
+	/**
+	 * Devuelve la cadena invertida
+	 * @return {String} la cadena invertida
+	 */
+	public String reverso() {
+		String cad = "";
+		for (int i=cadena.length()-1; i>=0; i--) {
+			cad += cadena.charAt(i);
+		}
+		return cad;
+	}
+		
+	/**
+	 * Devuelve la longitud de la cadena
+	 * @return {int} longitud de la cadena
+	 */
+	public int longi() {
+		return this.cadena.length();
+	}
+		
+	/**
+	 * Devuelve el número de vocales de la cadena
+	 * @return {int} número de vocales de la cadena
+	 */
+	public int voc() {
+		int conta = 0;
+		for (int i=0; i<this.cadena.length(); i++) {
+			char l = this.cadena.charAt(i);
+			if (l == 'a' || l == 'e' || l == 'i' || l == 'o' || l == 'u') {
+				conta++;
+			}
+		}
+		return conta;
+	}
+	
+	/**
+	 * Devuelve el número de consonantes de la cadena
+	 * @return {int} el número de consonantes del la cadena
+	 */
+	public int cons() {
+		int conta = 0;
+		for (int i=0; i<this.cadena.length(); i++) {
+			char c = this.cadena.charAt(i);
+			if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+				conta++;
+			}
+		}
+		return conta;
+	}
+	
+	/**
+	 * Devuelve el número de veces que aparece el caracter c en la cadena
+	 * @param c - caracter a comparar
+	 * @return {int} núnero de apariciones
+	 */
+	public int carac(char c) {
+		int conta = 0;
+		for (int i=0; i<this.cadena.length(); i++) {
+			char l = this.cadena.charAt(i);
+			c = Character.toUpperCase(c);
+			l = Character.toUpperCase(l);
+			if (l == c) {
+				conta++;
+			}
+		}
+		return conta;
+	}
+	
+	/**
+	 * Reemplaza el caracter introducido a por el caracter introducido b
+	 * @param a - caracter a quitar
+	 * @param b - caracter a establecer
+	 */
+	public void reemp(char a, char b) {
+		a = Character.toUpperCase(a);
+		b = Character.toUpperCase(b);
+		this.cadena = cadena.replace(a, b);
+	}
+	
+	/**
+	 * Elimina todas las apariciones del caracter pasado por parametro
+	 * @param c - caracter a eliminar
+	 */
+	public void borra(char c) {
+		this.cadena = cadena.replace(c, ' ');
+	}
 }
